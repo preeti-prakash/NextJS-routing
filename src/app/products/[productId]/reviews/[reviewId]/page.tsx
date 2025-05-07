f185a2ddc7d700decc14ff1cc6ff33a1a0ed959f
preeti-prakash <preetiprakashkandregula@gmail.com>
2025-05-07 12:56:04 -0400

    Custom Page not found and programmed - review and app

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,12 @@
+import { notFound } from "next/navigation";
 export default function ReviewDetails({
   params,
 }: {
   params: { reviewId: string; productId: string };
 }) {
+  if (parseInt(params.reviewId) > 1000) {
+    notFound();
+  }
   return (
     <h1>
       The review for product {params.productId} is {params.reviewId}
