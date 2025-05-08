f687c82ce48a9b529bd2ada282c473c875f65a51
preeti-prakash <preetiprakashkandregula@gmail.com>
2025-05-08 10:15:18 -0400

    static metadata configuration in page.tsx(about)

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,9 @@
 // the about router - http://localhost:3000/about
 
+// static metadata configuration in page.tsx
+export const metadata = {
+  title: "About Next.js basics",
+};
 export default function About() {
   return <h2>About Me.....</h2>;
 }
