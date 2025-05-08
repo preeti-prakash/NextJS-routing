f0c7ef3cc5901f399338e747ea55e916cdb2cc43
preeti-prakash <preetiprakashkandregula@gmail.com>
2025-05-08 10:37:11 -0400

    TITLE METADATA - ABSOLUTE, TEMPLATE, DEFAULT

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,11 @@
 // the profile router - http://localhost:3000/blog
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: {
+    absolute: "Blog",
+  },
+};
 export default function Blog() {
   return <h2>My Blog.....</h2>;
 }
