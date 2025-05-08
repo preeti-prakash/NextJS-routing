4c368ef125ba1eec3bd2b07a6326114b23557f0d
preeti-prakash <preetiprakashkandregula@gmail.com>
2025-05-08 10:25:06 -0400

    dynamic metadata function configuration in page.tsx([products]

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,3 +1,24 @@
+import { Metadata } from "next";
+
+type Props = {
+  params: {
+    productId: string;
+  };
+};
+// Dynamic metadata function  configuration
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iPhone ${params.productId}`);
+    }, 100);
+  });
+  return {
+    title: `Product ${title}`,
+  };
+};
+
 export default function ProductDetails({
   params,
 }: {
