09b5240a34d2e10e95c49e7a249bfe3be2bdf213
preeti-prakash <preetiprakashkandregula@gmail.com>
2025-05-08 11:16:51 -0400

    Navigation - Home, Products, Blog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 // the basic router - http://localhost:3000
-
+import Link from "next/link";
 export default function Home() {
-  return <h1>Welcome Home!!!</h1>;
+  return (
+    <>
+      <h1>Welcome Home!!!</h1>
+      <Link href="/blog">Blog</Link>
+      <br></br>
+      <Link href="/products">Products</Link>
+    </>
+  );
 }
